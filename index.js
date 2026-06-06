2e56e811745156310d6f0dd2c900ab55e1f515ad
DevanshMonish04 <devansh7895@gmail.com>
2026-06-06 22:07:52 +0530

    second commit

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.use("/api/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+  res.send('Task Manager API is running...');
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
